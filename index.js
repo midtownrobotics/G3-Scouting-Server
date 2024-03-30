6f4446ad1a680390f5b32a2290bd7139c276ece3
Gray JN <me@grayjn.com>
2024-03-30 13:23:22 -0400

    added authentication code

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const app = express();
 
 const PORT = 8080;
 
-app.use(express.static(__dirname + '/static'));
-
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    authenticate(req, res)
+    res.sendFile(__dirname + '/static/index.html');
 })
+
+function authenticate () {
+    const reject = () => {
+        res.setHeader("www-authenticate", "Basic");
+        res.sendStatus(401);
+    };
+    
+    const authorization = req.headers.authorization;
+    
+    if (!authorization) {
+        return reject();
+    }
+    
+    const [username, password] = Buffer.from(authorization.replace("Basic ", ""), "base64").toString().split(":");
+    
+    if (!(username === "g3" && password === "cowabunga")) {
+        return reject();
+    }
+}
 
 console.log(`listening on port ${PORT}!`)
 app.listen(PORT);
