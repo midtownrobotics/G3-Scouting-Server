4d6b3af38898909c84c516606b2e2a132b449739
Gray486 <me@grayjn.com>
2024-03-30 17:03:49 -0400

    added homepage, changed auth system

--- index.js
+++ index.js
@@ -3,29 +3,24 @@ const app = express();
 
 const PORT = 8080;
 
-app.get('/', (req, res) => {
-    authenticate(req, res)
-    res.sendFile(__dirname + '/static/index.html');
-})
+app.use(function(req, res, next) {
+    var auth;
 
-function authenticate () {
-    const reject = () => {
-        res.setHeader("www-authenticate", "Basic");
-        res.sendStatus(401);
-    };
-    
-    const authorization = req.headers.authorization;
-    
-    if (!authorization) {
-        return reject();
+    if (req.headers.authorization) {
+        auth = Buffer.from(req.headers.authorization.substring(6), 'base64').toString().split(':');
     }
-    
-    const [username, password] = Buffer.from(authorization.replace("Basic ", ""), "base64").toString().split(":");
-    
-    if (!(username === "g3" && password === "cowabunga")) {
-        return reject();
+
+    if (!auth || auth[0] !== 'g3' || auth[1] !== 'cowabunga') {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="G3"');
+        res.end('Unauthorized');
+    } else {
+        next();
     }
-}
+});
+
+
+app.use(express.static(__dirname + "/static"))
 
 console.log(`listening on port ${PORT}!`)
 app.listen(PORT);
